265c5e7754e1e96f2bd1185893f12e08447499db
Neha Gupta <ngupta647@gmail.com>
2019-10-01 22:23:24 +0530

    Updates

---
+++ precache-manifest.2af6ffcd765b12c3a9fde0586a223bee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99bc12167abcadd361b259570f6aa9bf",
+    "url": "/ExpenseTracker/index.html"
+  },
+  {
+    "revision": "2fe2deede6dc7fd83766",
+    "url": "/ExpenseTracker/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "7eb772b3306d8aacb98b",
+    "url": "/ExpenseTracker/static/js/2.bc334617.chunk.js"
+  },
+  {
+    "revision": "2fe2deede6dc7fd83766",
+    "url": "/ExpenseTracker/static/js/main.371c52f0.chunk.js"
+  },
+  {
+    "revision": "b06ea1be6bc423e018c9",
+    "url": "/ExpenseTracker/static/js/runtime~main.99e47884.js"
+  }
+]);
