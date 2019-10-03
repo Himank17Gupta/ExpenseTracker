43f2504852c9a3855627302bf7083e8b9a289364
Himank Gupta <ngupta647@gmail.com>
2019-10-03 10:02:34 +0530

    important build changes

--- src/models/actioncreator.js
+++ src/models/actioncreator.js
@@ -2,9 +2,28 @@ import React from 'react';
 
 //var expenseId=-1;                                 //firebase id conflict tobe handled later
 //localStorage.setItem('expenseId',expenseId);
+function uniqueNumber() {
+    var date = Date.now();
+   
+    if (date <= uniqueNumber.previous) {
+        date = ++uniqueNumber.previous;
+    } else {
+        uniqueNumber.previous = date;
+    }
+
+    return date;
+}
+
+uniqueNumber.previous = 0;
+
+function ID(){
+  return uniqueNumber();
+};
+
 function expenseActionCreator(expence,desc,amt,paid,type){
     console.log('localstorage:',localStorage.expenseId);
-var eid=localStorage.expenseId++;
+//var eid=localStorage.expenseId++;
+var eid=ID();
 console.log('localstorage:',localStorage.expenseId);
     return {
         payload:{expence,desc,amt,paid,eid},
